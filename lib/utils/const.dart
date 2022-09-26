00e5b9f4d095e0a633a3c3fa8ab2ec1ac94f5155
Dao Tran <daotran@ankvn.com>
2022-09-26 22:21:33 +0700

    update
    update

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,11 +1,10 @@
 class Constants {
   Constants._();
 
-  static const URL_BASE = "http://apicharging1.eappviet.com";
+  static const URL_BASE = "http://apicharging.takarastand.com/";
   static const TIME_OUT = 10000;
   static const AUTHORIZATION =
       "Basic VXNlckFQSU90b0NoYXJnaW5nOlBhc3NBUElPdG9DaGFyZ2luZw==";
   static const PAGE_LIMIT = 20;
-  static const APP_VERSION = "1.0.0";
-
+  static const APP_VERSION = "1.0.3";
 }
